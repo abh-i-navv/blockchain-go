e2ca805879078b87c82f3add7feadd495ed9f75f
abh-i-navv <abhinav11290@gmail.com>
2026-03-30 00:07:35 +0530

    fix api request parsing and mine handler responses

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,7 +1,8 @@
-package api
+﻿package api
 
 import (
 	"blockchain/blockchain"
+	"encoding/base64"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,21 +13,30 @@ type API struct {
 }
 
 type CreateTxRequest struct {
-	From   []byte  `json:"from"`
-	To     []byte  `json:"to"`
+	From   string  `json:"from"`
+	To     string  `json:"to"`
 	Amount float64 `json:"amount"`
 }
 
 type MineRequest struct {
-	Miner []byte `json:"miner"`
+	Miner string `json:"miner"`
 }
 
 type BalanceRequest struct {
-	Address []byte `json:"address"`
+	Address string `json:"address"`
 }
 
 func NewAPI(bc *blockchain.Blockchain) *API {
 	return &API{bc: bc}
+}
+
+func parseAddress(value string) []byte {
+	decoded, err := base64.StdEncoding.DecodeString(value)
+	if err == nil {
+		return decoded
+	}
+
+	return []byte(value)
 }
 
 func (api *API) GetBlocks(c *gin.Context) {
@@ -42,12 +52,12 @@ func (api *API) CreateTransaction(c *gin.Context) {
 	}
 
 	tx := blockchain.Transaction{
-		From:   req.From,
-		To:     req.To,
+		From:   parseAddress(req.From),
+		To:     parseAddress(req.To),
 		Amount: req.Amount,
 	}
 
-	api.bc.AddBlock([]blockchain.Transaction{tx}, req.From)
+	api.bc.AddBlock([]blockchain.Transaction{tx}, parseAddress(req.From))
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "transaction added",
@@ -61,9 +71,10 @@ func (api *API) Mine(c *gin.Context) {
 		c.JSON(400, gin.H{
 			"error": err.Error(),
 		})
+		return
 	}
 
-	api.bc.AddBlock([]blockchain.Transaction{}, req.Miner)
+	api.bc.AddBlock([]blockchain.Transaction{}, parseAddress(req.Miner))
 
 	c.JSON(200, gin.H{
 		"message": "block mined",
@@ -78,7 +89,7 @@ func (api *API) GetBalance(c *gin.Context) {
 		return
 	}
 
-	balance := api.bc.GetBalance(req.Address)
+	balance := api.bc.GetBalance(parseAddress(req.Address))
 
 	c.JSON(http.StatusOK, gin.H{
 		"balance": balance,
